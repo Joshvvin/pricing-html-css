fa01cfca5a1bb7cc3ba368c0ca9fd0f87c3281b2
Joshvvin Joshy <joshvvinjoshy@gmail.com>
2023-08-15 18:21:06 +0530

    Created annually.html to display annual pricing. - Created annual.js to direct back to index.html on the click event of toggle. - Created styles1.css to style the annually.html. - Created monthly.js to direct to the annually.html on the click event of toggle. - Made both the index.html and annually.html responsive.

---
+++ annual.js
@@ -0,0 +1,7 @@
+const tog = document.getElementById('slider');
+// console.log("hello world");
+// alert(tog.innerHTML);
+tog.addEventListener('click', function(){
+    window.location.replace('./index.html');   
+})
+// console.log("after click event")
---
+++ monthly.js
@@ -0,0 +1,7 @@
+const tog = document.getElementById('slider');
+// console.log("hello world");
+// alert(tog.innerHTML);
+tog.addEventListener('click', function(){
+    window.location.replace('./annually.html'); 
+})
+// console.log("after click event")
